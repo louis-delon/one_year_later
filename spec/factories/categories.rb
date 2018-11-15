c5421e35fe0393c398d7053b59ea85ca54e755e6
LouisDelon <louleman34@gmail.com>
2018-11-15 06:45:12 +0100

    create category table / write test / run migration

---
+++ spec/factories/categories.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :category do
+    name { "MyString" }
+    weigth { 1 }
+    user { nil }
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,36 +25,4 @@ RSpec.describe User, type: :model do
 end
 
 
-# require 'rails_helpers'
-
-# RSpec.describe Category, type: :model do
-
-#   it "is not valid without a name" do
-#    category = Category.new(name: nil, weigth: 1)
-#    expect(category).to_not be_valid
-#   end
-
-#   it 'should persist a category' do
-#     Category.create(name: 'test', weight: 1)
-#     expect Category.count.to(eq(1))
-#   end
-
-#   it 'should have association with category table', type: :model do
-#     user = User.create(email: 'louis@holdies.com', password: 'password', username: 'loulou')
-#     category1 = Category.create(name: 'louis', weigth: 1)
-#     category2 = Category.create(name: 'delon', weigth: 2)
-#     expect (user.categories).to be_an_instance_of(Array)
-#     expect (user.categories.count).to eq(2)
-#   end
-
-#   it 'should destroy categories if user is destroyed'
-#     user = User.create(email: 'louis@holdies.com', password: 'password', username: 'loulou')
-#     category1 = Category.create(name: 'louis', weigth: 1)
-#     category2 = Category.create(name: 'delon', weigth: 2)
-#     user.destroy
-#     expect(category1).to be_nil
-#     expect(category2).to be_nil
-#   end
-# end
-
 
