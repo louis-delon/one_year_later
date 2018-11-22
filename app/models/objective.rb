61a67f45eafea8c2a460884c4b2b5adccf95506b
LouisDelon <louleman34@gmail.com>
2018-11-23 00:19:26 +0100

    manage rspec objectives

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,3 +1,5 @@
 class Objective < ApplicationRecord
   belongs_to :state
+  has_many :actions, dependent: :destroy
+  validates :title, :description, :deadline, presence: true
 end
--- spec/models/objective_spec.rb
+++ spec/models/objective_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Objective, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "objective associations" do
+    it { should belong_to(:state) }
+    it { should have_many(:actions).dependent(:destroy) }
+  end
+
+  context "objective validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:deadline) }
+  end
+
+  context "objective persistence" do
+    it 'should persist a objective' do
+      category = build(:category)
+      state = build(:state)
+      objective = build(:objective)
+      category.states << state
+      state.objectives << objective
+      category.save!
+      expect(Objective.count).to eq(1)
+    end
+  end
 end
